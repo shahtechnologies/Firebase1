bd65854c23a90e4234a96c7ad464891dfd7b669f
shahtechnologies <info.shahtechnologies@gmail.com>
2017-02-06 15:58:23 +0530

    Initial commit

---
+++ app/src/main/java/shahtechnologies/net/firebase/Config.java
@@ -0,0 +1,11 @@
+package shahtechnologies.net.firebase;
+
+
+/**
+ * Created by Belal on 2/23/2016.
+ */
+public class Config {
+    public static final String FIREBASE_URL = "https://test-22bf2.firebaseio.com/";
+
+
+}
